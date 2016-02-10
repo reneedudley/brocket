58fb8cf729ff30f49c56625d3c41bf88142f3c63
reneedudley <4reneedudley@gmail.com>
2016-02-09 19:31:20 -0600

    fixed some migrations

--- database/migrations/2016_02_10_003735_create_bookmarkTag_table.php
+++ database/migrations/2016_02_10_003735_create_bookmarkTag_table.php
@@ -16,7 +16,7 @@ class CreateBookmarkTagTable extends Migration
             $table->increments('id');
             $table->timestamps();
             $table->integer('user_id')->unsigned();
-            $table->integer('subbreddit_id')->unsigned();
+            $table->integer('bookmark_tag')->unsigned();
         });
     }
 
